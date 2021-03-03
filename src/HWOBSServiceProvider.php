ac29c2b603638a7864a6f54a9de995bdaca72a6a
wangqs <wqsacy@vip.qq.com>
2021-03-04 01:36:26 +0800

    初始化

--- src/HWOBSServiceProvider.php
+++ src/HWOBSServiceProvider.php
@@ -1,59 +1,62 @@
 <?php
 
-namespace Wangqs\HuaweiOBS;
+	namespace Wangqs\HuaweiOBS;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Container\Container;
-use Illuminate\Foundation\Application as LaravelApplication;
-use Illuminate\Support\Facades\Storage;
-use Laravel\Lumen\Application as LumenApplication;
-use Wangqs\ObsV3\ObsClient;
-use League\Flysystem\Filesystem;
-class HWOBSServiceProvider extends ServiceProvider
-{
-    public function boot()
-    {
-        $this->setUpConfig();
-        $this->setStorage();
-    }
+	use Illuminate\Support\ServiceProvider;
+	use Illuminate\Container\Container;
+	use Illuminate\Foundation\Application as LaravelApplication;
+	use Illuminate\Support\Facades\Storage;
+	use Laravel\Lumen\Application as LumenApplication;
+	use Wangqs\ObsV3\ObsClient;
+	use League\Flysystem\Filesystem;
 
-    public function register()
-    {
-        $app = $this->app;
+	class HWOBSServiceProvider extends ServiceProvider
+	{
+		public function boot () {
+			$this->setUpConfig();
+			$this->setStorage();
+		}
 
-        $app->singleton('hwobs.factory', function($app) {
-            return new Factory();
-        });
+		public function register () {
+			$app = $this->app;
 
-        $app->singleton('hwobs', function($app) {
-            return new Manager($app, $app['hwobs.factory']);
-        });
+			$app->singleton( 'hwobs.factory' , function ( $app )
+			{
+				return new Factory();
+			} );
 
-        $app->alias('hwobs', Manager::class);
+			$app->singleton( 'hwobs' , function ( $app )
+			{
+				return new Manager( $app , $app['hwobs.factory'] );
+			} );
 
-        $app->singleton(ObsClient::class, function(Container $app) {
-            return $app->make('hwobs')->bucket();
-        });
+			$app->alias( 'hwobs' , Manager::class );
 
-    }
+			$app->singleton( ObsClient::class , function ( Container $app )
+			{
+				return $app->make( 'hwobs' )
+				           ->bucket();
+			} );
 
-    protected function setUpConfig(): void
-    {
-        $source = __DIR__ . '/config/hwobs.php';
-        if ($this->app instanceof LaravelApplication) {
-            $this->publishes([$source => config_path('hwobs.php')], 'config');
-        } elseif ($this->app instanceof LumenApplication) {
-            $this->app->configure('hwobs');
-        }
-        $this->mergeConfigFrom($source, 'hwobs');
-    }
+		}
+
+		protected function setUpConfig () : void {
+			$source = __DIR__ . '/config/hwobs.php';
+			if ( $this->app instanceof LaravelApplication ) {
+				$this->publishes( [ $source => config_path( 'hwobs.php' ) ] , 'config' );
+			}
+			else if ( $this->app instanceof LumenApplication ) {
+				$this->app->configure( 'hwobs' );
+			}
+			$this->mergeConfigFrom( $source , 'hwobs' );
+		}
 
 
-    protected function setStorage()
-    {
-        Storage::extend('hwobs', function ($app,$config) {
-            $client = ObsClient::factory($config);
-            return new Filesystem(new HwobsAdapter($client,'',$config['bucket']));
-        });
-    }
-}
+		protected function setStorage () {
+			Storage::extend( 'hwobs' , function ( $app , $config )
+			{
+				$client = ObsClient::factory( $config );
+				return new Filesystem( new HwobsAdapter( $client , '' , $config['bucket'] ) );
+			} );
+		}
+	}
